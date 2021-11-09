33f9a7b1e19ad6ad61041530ffb96faaefc0e0f1
OnoweMalya <onowemalya@gmail.com>
2021-11-09 20:07:06 -0300

    included all files

---
+++ src/config.js
@@ -0,0 +1,14 @@
+global.SALT_KEY = '2ade2c05-758d-4afb-ba5d-93a1d351a73c';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://onowemalya:b22vkv6p@onowemalya.w5vet.mongodb.net/onowemalya',
+    sendgridKey: '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',
+    containerConnectionString: 'SUA CONNECTION STRING'
+}
+
+/*
+https://app.sendgrid.com/
+
+
+*/
---
+++ src/routes/index-route.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.2"
+    });
+});
+
+module.exports = router; /* para referencia as aplicações entres os sheets */
+
+/*
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/usuarios', function(req, res){
+  res.render('users', {
+    users: users,
+    title: "EJS example",
+    header: "Some users"
+  });
+});
+
+
+*/
